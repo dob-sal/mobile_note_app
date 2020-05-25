1faa4daee6742b51fcd204a53317725a69dbdb49
dob_sal <dobrin.salabashev@fourth.com>
2020-05-25 20:18:18 +0300

    Added sign in form bloc structure

---
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:meta/meta.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+import 'package:mobile_note_app/domain/auth/value_objects.dart';
+import 'package:mobile_note_app/domain/auth/auth_failure.dart';
+import 'package:mobile_note_app/domain/auth/i_auth_facade.dart';
+
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+
+part 'sign_in_form_bloc.freezed.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final IAuthFacade _authFacade;
+
+  SignInFormBloc(this._authFacade);
+
+  @override
+  SignInFormState get initialState => SignInFormState.initial();
+
+  @override
+  Stream<SignInFormState> mapEventToState(
+    SignInFormEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
